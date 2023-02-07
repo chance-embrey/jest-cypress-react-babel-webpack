5ce30a9d376031c72e76e104d6cedad4885aebd6
Chance Embrey <cembreyfarquhar@leantechniques.com>
2023-02-07 15:57:15 -0600

    Add jest config file; add test file for calculator

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
+  moduleNameMapper: {
+    '\\.module.css$': 'identity-obj-proxy',
+    '\\.css$': require.resolve('./test/style-mock.js'),
+  },
+}
